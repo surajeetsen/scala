a0339e08e836c13828bdd65cc50fdf2aad3750ab
Surajeet Sen <surajeetsen@yahoo.co.in>
2016-12-22 13:10:53 +0530

    Refactored code

--- scala-algos/src/sorting/InsertionSort.scala
+++ scala-algos/src/sorting/InsertionSort.scala
@@ -2,30 +2,37 @@ package sorting
 
 /**
  * Scala program for implementation of Insertion Sort
- * 
+ *
  * @author Surajeet Sen
  */
 object InsertionSort {
-  
-  def sort(input: Array[Int]) : Array[Int] = {
+
+  def sort(input: Array[Int]): Array[Int] = {
+    //Base condition
+    if(input == null || input.length <= 1) {
+      return input
+    }
     
-    for(i <- 1 to input.length-1) {
-      for(j <- i to 1 by -1) {
-        if(input(j) < input(j-1)) {
-          var temp = input(j-1)
-          input(j-1) = input(j)
+    for (i <- 1 to input.length - 1) {
+      var isSorted: Boolean = false;
+      for (j <- i to 1 by -1 if !isSorted) {
+        if (input(j) > input(j - 1)) {
+          isSorted = true
+        } else {
+          var temp = input(j - 1)
+          input(j - 1) = input(j)
           input(j) = temp
         }
       }
     }
-    
+
     input
   }
-  
-  def main(args : Array[String]) {
-    var input : Array[Int] = Array(7, 4, 1, 3, 2, 6, 5, 9, 10, 8)
+
+  def main(args: Array[String]) {
+    var input: Array[Int] = Array(7, 4, 1, 3, 2, 6, 5, 9, 10, 8)
     sort(input)
-    for(i <- input) { print(i+ " ") }
+    for (i <- input) { print(i + " ") }
   }
-  
+
 }
